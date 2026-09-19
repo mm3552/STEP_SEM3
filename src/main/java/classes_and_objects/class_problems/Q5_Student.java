d69681f43c7983b1d6886ddee8cc144c1b2101ef
M Aditya <mm3552@srmist.edu.in>
2026-09-19 15:53:56 +0530

    Add Week 6 Classes and Objects problem

---
+++ src/main/java/classes_and_objects/class_problems/Q5_Student.java
@@ -0,0 +1,27 @@
+package classes_and_objects.class_problems;
+
+class Student {
+    String name;
+    double attendance;
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        new Student("Ravi", 92.5);
+        new Student("Anitha", 95.0);
+
+        System.out.println("2 Student objects created");
+        Student.printCollegeInfo();
+    }
+}

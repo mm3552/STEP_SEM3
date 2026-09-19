e9bd80cee1e354c11350d61640768a877d763444
M Aditya <mm3552@srmist.edu.in>
2026-09-19 16:31:32 +0530

    Add Week 7 Encapsulation problem

---
+++ src/main/java/encapsulation_and_access_control/assigment_problems/Q2_Playlist.java
@@ -0,0 +1,37 @@
+package encapsulation_and_access_control.assigment_problems;
+
+import java.util.Arrays;
+
+class Playlist {
+    private final String[] songs;
+    private int count;
+
+    Playlist(int maximumSize) {
+        songs = new String[maximumSize];
+    }
+
+    void addSong(String song) {
+        if (count < songs.length) {
+            songs[count] = song;
+            count++;
+        }
+    }
+
+    String[] getSongs() {
+        return Arrays.copyOf(songs, count);
+    }
+
+    int getSongCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Playlist p = new Playlist(10);
+        p.addSong("Song A");
+        p.addSong("Song B");
+        String[] copy = p.getSongs();
+        copy[0] = "Hacked";
+        System.out.println(p.getSongs()[0]);
+        System.out.println(p.getSongCount());
+    }
+}
